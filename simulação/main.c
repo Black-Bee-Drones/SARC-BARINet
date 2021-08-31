e5f7d20d0f060d5049d8c1b8ac27ccd874b80d13
Ryan_Sil <83303260+RyanFoward@users.noreply.github.com>
2021-08-31 14:06:05 -0300

    Master do Código para Simulação

---
+++ simulação/main.c
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import asyncio
+from mavsdk import System
+
+
+async def run():
+
+    drone1 = System(mavsdk_server_address="localhost", port=50040)
+    drone2 = System(mavsdk_server_address="localhost", port=50041)
+    drone3 = System(mavsdk_server_address="localhost", port=50042)
+
+    await drone1.connect(system_address="udp://:14540")
+    await drone2.connect(system_address="udp://:14541")
+    await drone3.connect(system_address="udp://:14542")
+    
+    print("Waiting for drone 1 to connect...")
+    async for state in drone1.core.connection_state():
+        if state.is_connected:
+            print(f"Drone 1 discovered!")
+            break
+
+    print("Waiting for drone 2 to connect...")
+    async for state in drone2.core.connection_state():
+        if state.is_connected:
+            print(f"Drone 2 discovered!")
+            break
+
+    print("Waiting for drone 3 to connect...")
+    async for state in drone3.core.connection_state():
+        if state.is_connected:
+            print(f"Drone 3 discovered!")
+            break
+
+    print("-- Arming")
+    await drone1.action.arm()
+
+    print("-- Arming 2 ")
+    await drone2.action.arm()
+
+    print("-- Arming 3 ")
+    await drone3.action.arm()
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run())
