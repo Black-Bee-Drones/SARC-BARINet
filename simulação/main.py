c74bfcfd415dca06b02784aea573a3a3359ffa44
Ryan <ryanaugustoo71@hotmail.com>
2021-08-31 15:53:19 -0300

    Commit de teste retirado

--- simulação/main.py
+++ simulação/main.py
@@ -1,7 +1,6 @@
 
 import asyncio
 from mavsdk import System
-from mavsdk import telemetry
 
 
 async def run():
@@ -14,8 +13,6 @@ async def run():
     await drone2.connect(system_address="udp://:14541")
     await drone3.connect(system_address="udp://:14542")
 
-    print(f"{drone1.telemetry.health}, {drone2.telemetry.health}, {drone3.telemetry.health}"
-    
     print("Waiting for drone 1 to connect...")
     async for state in drone1.core.connection_state():
         if state.is_connected:
